871479b4e164d1dcb3f5f7820e99ad1978c95962
JigmeTenzinChogyel <jigmetenzinchogyel@gmail.com>
2024-08-01 20:21:40 +0600

    Added landing page image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import campBg from "../../public/landing/camp-bg.avif";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="">
+      <Image src={campBg} alt="" fill />
+    </main>
+  );
 }
